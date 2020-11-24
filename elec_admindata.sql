eda8da7679ac9704020407e539734e9f494be909
parth700 <parthchaturvedi700@gmail.com>
2020-11-24 16:38:27 +0530

    changed command

--- elec_admindata.sql
+++ elec_admindata.sql
@@ -4,5 +4,5 @@ delete from votes;
 delete from winner;
 delete from admin;
 delete from election;
-insert into election values('elec1', 'not_started', '2020-12-10 10:00:01', '2020-12-10 17:59:59', 'this is the guidline of the election: \n 1)- guid1\n 2)- guid2');
+insert into election values('elec1', 'not_started', '2020-12-10 10:00:01', '2020-12-10 17:59:59', E'this is the guidline of the election:\n 1)- guid1\n 2)- guid2');
 insert into admin values('18675', 'elec1');
